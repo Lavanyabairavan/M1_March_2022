27c507e10281fa4b9b045a5a56d49d67d6ca4659
Lavanyabairavan <101272689+Lavanyabairavan@users.noreply.github.com>
2022-04-02 12:31:34 +0530

    Update Banking.c

--- 3_Implementation/src/Banking.c
+++ 3_Implementation/src/Banking.c
@@ -32,9 +32,9 @@ int main(){
 	else if(opt == 2){
 		system("clear");
 		printf("\nPhone No.:\t");
-		scanf("%49s",phone);
+		scanf("%50s",phone);
 		printf("Password:\t");
-		scanf("%49s",password);
+		scanf("%50s",password);
 		fp = fopen(strcat(phone,".dat"),"r");
 		if(fp == NULL) printf("Account number not registered");
 		else {
